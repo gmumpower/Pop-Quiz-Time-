a392efb1ada11d12029d0895509eb85e373bee56
Gavin M <gavinmmupower96@gmail.com>
2022-01-27 00:03:25 -0500

    pushing main project

---
+++ script.js
@@ -0,0 +1,18 @@
+var time = document.querySelector(".timer");
+var score = document.querySelector("#score");
+
+var start = document.querySelector("#start-button");
+var secondsRemaining = 60;
+
+
+
+
+
+
+
+function startQuiz() {
+    intro.style.display = "none";
+    questionCount = 0;
+    setTimeout();
+    setQuestion();
+}
